31880e1f031b600ab2c6867c642e91986e64b76b
Ankita Raj <95190026+ankiraj09@users.noreply.github.com>
2024-02-23 19:16:23 +0530

    dicesimulator.py

--- dicesimulator.py
+++ dicesimulator.py
@@ -1,1 +1,19 @@
-import tkinter
+import random
+x = "y"
+while x == "y":
+no = random.radint(1,6)
+if x == 1:
+print("[0]")
+if x == 2:
+print("[0 0]")
+if x == 3:
+print("[0 0 0]")
+if x == 4:
+print("[0 0 0 0]")
+if x == 5:
+print("[0 0 0 0 0]")
+if x == 6:
+print("[0 0 0 0 0 0]")
+
+x = input("press y to roll again and n to exit:")
+print("\n")
